3e815ed837918b512bdc402c0806fa8f2dd8e13f
Tommaso <tommaso.brini@studio.unibo.it>
2024-04-24 17:58:07 +0200

    Update GUI

--- src/ex1/inspector/StartStopView.java
+++ src/ex1/inspector/StartStopView.java
@@ -11,18 +11,22 @@ import java.util.List;
 public class StartStopView extends JPanel {
     private static final String START = "Start";
     private static final String PAUSE = "Pause";
+    private static final String RESUME = "Resume";
     public static final String RESET = "Reset";
+    public static final String STOP = "Stop";
     private final JButton startButton;
-    private final JButton pauseButton;
+    private final JButton pauseResumeButton;
     private final JButton resetButton;
+    private final JButton stopButton;
     private final FlowLayout layoutManager;
     private final List<StartStopViewListener> listeners;
     private boolean isSetup;
 
     public StartStopView() {
         this.startButton = new JButton(START);
-        this.pauseButton = new JButton(PAUSE);
+        this.pauseResumeButton = new JButton(PAUSE);
         this.resetButton = new JButton(RESET);
+        this.stopButton = new JButton(STOP);
         this.layoutManager = new FlowLayout(FlowLayout.CENTER);
         this.listeners = new ArrayList<>();
 
@@ -30,8 +34,9 @@ public class StartStopView extends JPanel {
         this.setLayout(this.layoutManager);
         this.setBackground(ViewUtils.GUI_BACKGROUND_COLOR);
         this.add(this.startButton);
-        this.add(this.pauseButton);
+        this.add(this.pauseResumeButton);
         this.add(this.resetButton);
+        this.add(this.stopButton);
         this.activateStartButton();
 
         this.isSetup = false;
@@ -46,30 +51,43 @@ public class StartStopView extends JPanel {
         this.startButton.setEnabled(false);
     }
 
+    private void activatePauseButton() {
+        this.pauseResumeButton.setEnabled(true);
+    }
+
+    private void deactivatePauseButton() {
+        this.pauseResumeButton.setEnabled(false);
+    }
+
     private void activateStopButton() {
-        this.pauseButton.setEnabled(true);
+        this.stopButton.setEnabled(true);
     }
 
     private void deactivateStopButton() {
-        this.pauseButton.setEnabled(false);
+        this.stopButton.setEnabled(false);
     }
 
     private void graphicsSetup() {
         this.deactivateStartButton();
+        this.deactivatePauseButton();
         this.deactivateStopButton();
         this.startButton.setBackground(Color.green);
-        this.pauseButton.setBackground(Color.red);
+        this.pauseResumeButton.setBackground(Color.yellow);
+        this.stopButton.setBackground(Color.red);
     }
 
     private void switchStop() {
         this.deactivateStartButton();
+        this.activatePauseButton();
         this.activateStopButton();
     }
 
-    private void switchStart() {
-        this.deactivateStopButton();
-        this.activateStartButton();
-    }
+
+//    private void switchStart() {
+//        this.deactivatePauseButton();
+//        this.activateStartButton();
+//    }
+
 
     public void addListener(final StartStopViewListener listener) {
         this.listeners.add(listener);
@@ -87,9 +105,15 @@ public class StartStopView extends JPanel {
             System.out.println("PLAY");
             this.switchStop();
         });
-        this.pauseButton.addActionListener(e -> {
-            simulation.startStopMonitor().pause();
-            this.switchStart();
+        this.pauseResumeButton.addActionListener(e -> {
+            if (this.pauseResumeButton.getText().equals(PAUSE)) {
+                simulation.startStopMonitor().pause();
+                this.pauseResumeButton.setText(RESUME);
+            } else {
+                //esegue come se fosse start
+                simulation.startStopMonitor().play();
+                this.pauseResumeButton.setText(PAUSE);
+            }
         });
 //        this.resetButton.addActionListener(e -> {
 //            this.isSetup = false;
@@ -97,11 +121,20 @@ public class StartStopView extends JPanel {
 //            this.switchStart();
 //            this.resetButton.setVisible(false);
 //        });
+        this.stopButton.addActionListener(e -> {
+            simulation.startStopMonitor().pause();
+            this.onEndSimulation();
+            JOptionPane.showMessageDialog(this, "Simulation closed");
+            System.exit(0);
+        });
     }
 
     public void onEndSimulation() {
 //        this.startButton.setEnabled(false);
 //        this.pauseButton.setEnabled(false);
 //        this.resetButton.setVisible(true);
+        this.deactivatePauseButton();
+        this.deactivateStartButton();
+        this.deactivateStopButton();
     }
 }
