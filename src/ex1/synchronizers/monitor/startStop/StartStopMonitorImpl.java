698d1c314e9deea8d9db30f79175a76a77806abb
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-04-26 13:45:26 +0200

    fix: Clean code

--- src/ex1/synchronizers/monitor/startStop/StartStopMonitorImpl.java
+++ src/ex1/synchronizers/monitor/startStop/StartStopMonitorImpl.java
@@ -15,15 +15,6 @@ public class StartStopMonitorImpl implements StartStopMonitor {
         this.isAwaiting = false;
     }
 
-    private void setAwaiting(final boolean isAwaiting) {
-        try {
-            this.mutex.lock();
-            this.isAwaiting = isAwaiting;
-        } finally {
-            this.mutex.unlock();
-        }
-    }
-
     @Override
     public void play() {
         try {
