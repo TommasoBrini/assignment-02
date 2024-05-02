b3407311b942b8ab91ef894cd0d059ef967f95cd
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-05-02 17:44:06 +0200

    Complete test server

--- test/testServer/TestServer.java
+++ test/testServer/TestServer.java
@@ -13,6 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TestServer {
+    private static final String JAVADOC_PATH = "res/docs/docs/api";
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
         HttpServer server = vertx.createHttpServer();
@@ -20,11 +21,9 @@ public class TestServer {
         Router router = Router.router(vertx);
 
         // Gestisce le richieste per la Javadoc
-        router.get("/javadoc/*").handler(ctx -> {
-            HttpServerRequest request = ctx.request();
-            String filePath = request.path().substring("/javadoc".length());
+        router.get("/index.html").handler(ctx -> {
             try {
-                String javadoc = getJavadoc(filePath);
+                String javadoc = getJavadoc();
                 ctx.response()
                         .putHeader("content-type", "text/html")
                         .end(javadoc);
@@ -40,8 +39,8 @@ public class TestServer {
         System.out.println("Server started on port 8080");
     }
 
-    private static String getJavadoc(String filePath) throws IOException {
-        Path path = Paths.get(filePath);
+    private static String getJavadoc() throws IOException {
+        Path path = Paths.get(JAVADOC_PATH, "/index.html");
         if (Files.exists(path) && Files.isRegularFile(path) && path.toString().endsWith(".html")) {
             return new String(Files.readAllBytes(path));
         } else {
