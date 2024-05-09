86e986d8946bb890fc52fc58bb06f0e67b71806f
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-05-09 11:34:21 +0200

    feat: green findRemoteUrls

--- test/core/SearchLogicTest.java
+++ test/core/SearchLogicTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 public class SearchLogicTest {
     private static final String LOCAL_URL = Server.LOCAL_PATH + "index.html";
+    private static final String REMOTE_URL = "https://www.google.com";
     private final Server server = new Server();
     private SearchLogic searchLogic;
     private ClientService clientService;
@@ -18,20 +19,21 @@ public class SearchLogicTest {
     @Before
     public void setUp() {
         this.server.run();
-        this.searchLogic = SearchLogicFactory.createLocal();
         this.clientService = ClientServiceFactory.createJsoup();
     }
 
     @Test
     public void findLocalUrls() {
+        this.searchLogic = SearchLogicFactory.createLocal();
         final Document document = this.clientService.onSearch(LOCAL_URL);
         this.searchLogic.findUrls(document).forEach(System.out::println);
     }
 
-//    @Test
-//    public void findLocalUrlas() {
-////        final Document
-////        this.searchLogic.findUrls(null).forEach(System.out::println);
-//    }
+    @Test
+    public void findRemoteUrls() {
+        this.searchLogic = SearchLogicFactory.createRemote();
+        final Document document = this.clientService.onSearch(REMOTE_URL);
+        this.searchLogic.findUrls(document).forEach(System.out::println);
+    }
 
 }
