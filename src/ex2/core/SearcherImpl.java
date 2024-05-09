d8cf6c8a820610bd6dca505846860a7cba8b2929
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-05-09 14:56:28 +0200

    feat: green remoteSearch

---
+++ src/ex2/core/SearcherImpl.java
@@ -0,0 +1,45 @@
+package ex2.core;
+
+import ex2.web.client.ClientService;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SearcherImpl implements Searcher {
+    private final String word;
+    private int totalWord;
+    private long start;
+
+    public SearcherImpl(final String word) {
+        this.word = word;
+        this.totalWord = 0;
+    }
+
+    private int findWord(final Document document) {
+        final Elements texts = document.select("body");
+        return (int) texts.stream().map(Element::text)
+                .flatMap(text -> Arrays.stream(text.split("\\s+")))
+                .filter(word -> word.equals(this.word)).count();
+    }
+
+    @Override
+    public List<String> initSearch(final ClientService clientService, final SearchLogic.Type searchLogicType, final String url) {
+        this.start = System.currentTimeMillis();
+        return this.search(clientService, searchLogicType, url);
+    }
+
+    @Override
+    public List<String> search(final ClientService clientService, final SearchLogic.Type searchLogicType, final String url) {
+        final Document document = clientService.findUrl(url);
+        this.totalWord += this.findWord(document);
+        return SearchLogicFactory.create(searchLogicType).findUrls(document);
+    }
+
+    @Override
+    public int totalWord() {
+        return this.totalWord;
+    }
+}
--- test/core/SearcherTest.java
+++ test/core/SearcherTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static ex2.utils.PathUtils.LOCAL_URL;
+import static ex2.utils.PathUtils.REMOTE_URL;
 
 public class SearcherTest {
     private final Server server = new Server();
@@ -22,15 +23,24 @@ public class SearcherTest {
     @Before
     public void setup() {
         this.server.run();
-        this.word = "Java";
-        this.searcher = new SearcherImpl(this.word);
         this.clientService = ClientServiceFactory.createVertx();
     }
 
     @Test
     public void localSearch() {
+        this.word = "Java";
+        this.searcher = new SearcherImpl(this.word);
         final List<String> urls = this.searcher.initSearch(this.clientService, SearchLogic.Type.LOCAL, LOCAL_URL);
         urls.forEach(url -> this.searcher.search(this.clientService, SearchLogic.Type.LOCAL, url));
+        System.out.println(this.searcher.totalWord());
+    }
+
+    @Test
+    public void remoteSearch() {
+        this.word = "Google";
+        this.searcher = new SearcherImpl(this.word);
+        final List<String> urls = this.searcher.initSearch(this.clientService, SearchLogic.Type.REMOTE, REMOTE_URL);
+        urls.forEach(url -> this.searcher.search(this.clientService, SearchLogic.Type.REMOTE, url));
         System.out.println(this.searcher.totalWord());
     }
 }
