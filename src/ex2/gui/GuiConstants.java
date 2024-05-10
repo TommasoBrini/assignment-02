6ed0fbc58f0ca6ff7ca8fe61f987606ba8136d35
Tommaso <tommaso.brini@studio.unibo.it>
2024-05-10 14:24:12 +0200

    core: add combobox with total word counter

--- src/ex2/gui/GuiConstants.java
+++ src/ex2/gui/GuiConstants.java
@@ -6,7 +6,7 @@ public final class GuiConstants {
     // DIMENSION
     public static final Dimension FRAME_DIMENSION = new Dimension(800, 500);
     public static final Dimension PRINT_AREA_DIMENSION = new Dimension(380, 380);
-    public static final Dimension HISTORY_AREA_DIMENSION = new Dimension(350, 450);
+    public static final Dimension HISTORY_AREA_DIMENSION = new Dimension(400, 450);
 
     // BUTTON
     public static final int HISTORY_BUTTON_HEIGHT = 65;
@@ -15,5 +15,6 @@ public final class GuiConstants {
     public static final String URL = "URL: ";
     public static final String WORD = "Word: ";
     public static final String DEPTH = "Depth: ";
+    public static final String TOTAL_WORDS = "Total words: ";
 
 }
