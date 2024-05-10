2e99e796bffacef82d1af93b8b9dce4fc1a550eb
Tommaso <tommaso.brini@studio.unibo.it>
2024-05-10 12:28:43 +0200

    core: implement ConcurrentSet

---
+++ src/ex2/utils/ConcurrentSet.java
@@ -0,0 +1,43 @@
+package ex2.utils;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentSet<T> {
+    private Set<T> set;
+    private Lock lock;
+
+    public ConcurrentSet() {
+        this.set = new HashSet<>();
+        this.lock = new ReentrantLock();
+    }
+
+    public boolean add(T element) {
+        lock.lock();
+        try {
+            return set.add(element);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean contains(T element) {
+        lock.lock();
+        try {
+            return set.contains(element);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int size() {
+        lock.lock();
+        try {
+            return set.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
