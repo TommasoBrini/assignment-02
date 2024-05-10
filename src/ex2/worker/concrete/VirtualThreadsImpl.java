d637cabe0140985cc78ade5be4b729297daa7d14
Tommaso <tommaso.brini@studio.unibo.it>
2024-05-10 18:16:34 +0200

    core: implements VirtualThreadsImpl with future

--- src/ex2/worker/concrete/VirtualThreadsImpl.java
+++ src/ex2/worker/concrete/VirtualThreadsImpl.java
@@ -2,13 +2,13 @@ package ex2.worker.concrete;
 
 import ex2.core.event.SearchResponse;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 public class VirtualThreadsImpl extends AbstractWorker {
     private final ExecutorService executor;
-
     public VirtualThreadsImpl() {
         this.executor = Executors.newVirtualThreadPerTaskExecutor();
     }
@@ -20,10 +20,54 @@ public class VirtualThreadsImpl extends AbstractWorker {
 
     @Override
     public void addEventUrl(final SearchResponse response) {
-        this.executor.submit(() -> {
+        Future<?> future = executor.submit(
+                () -> {
+                    final List<SearchResponse> responses = this.searcher().search(response);
+                    this.onResponseView(response);
+
+                    IntStream.range(0, response.maxDepth()).forEach(i ->
+                            responses.forEach(res -> {
+                                System.out.println(res);
+                                //mappo tutti i response in una lista di future
+                                final List<Future<?>> futures = new ArrayList<>();
+                                futures.add(this.executor.submit(() -> {
+                                    final List<SearchResponse> list = this.searcher().search(res);
+                                    list.forEach(this::onResponseView);
+                                    System.out.println(list.size());
+                                    System.out.println("Depth: " + res.currentDepth());
+                                }));
+                                //controllo che le futures siano terminate
+                                this.waitForAllFutures(futures);
+                            })
+                    );
+                }
+        );
+
+        try {
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            this.onFinishListener();
+        }
+        //this.waitForAllFutures(List.of(future));
+
+                /*List.of(this.executor.submit(() -> {
             final List<SearchResponse> responses = this.searcher().search(response);
             this.onResponseView(response);
             responses.forEach(this::addEventUrl);
+        }));
+        //controllo che le futures siano terminate
+        this.waitForAllFutures(futures);*/
+    }
+
+    private void waitForAllFutures(final List<Future<?>> futures) {
+        futures.forEach(future -> {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
         });
     }
 
