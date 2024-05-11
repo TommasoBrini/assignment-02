45546bc805129ec1351faeecb6d19b95981feca3
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-05-11 09:05:00 +0200

    refactor: clean up VirtualThreadsImpl

--- src/ex2/worker/concrete/VirtualThreadsImpl.java
+++ src/ex2/worker/concrete/VirtualThreadsImpl.java
@@ -2,10 +2,8 @@ package ex2.worker.concrete;
 
 import ex2.core.event.SearchResponse;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class VirtualThreadsImpl extends AbstractWorker {
@@ -22,11 +20,11 @@ public class VirtualThreadsImpl extends AbstractWorker {
 
     @Override
     public void addEventUrl(final SearchResponse response) {
-        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+        final CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
             final List<SearchResponse> responses = this.searcher().search(response);
             responses.forEach(this::onResponseView);
 
-            List<CompletableFuture<Void>> nestedFutures = responses.stream()
+            final List<CompletableFuture<Void>> nestedFutures = responses.stream()
                     .flatMap(res -> IntStream.range(0, res.maxDepth())
                             .mapToObj(i -> CompletableFuture.runAsync(() -> {
                                 this.onResponseView(res);
@@ -34,27 +32,15 @@ public class VirtualThreadsImpl extends AbstractWorker {
                                 list.forEach(this::onResponseView);
                                 System.out.println(list.size());
                                 System.out.println("Depth: " + res.currentDepth());
-
-
                             }, this.executor))
                     )
                     .toList();
 
-            CompletableFuture<Void> allNestedFutures = CompletableFuture.allOf(nestedFutures.toArray(new CompletableFuture[0]));
+            final CompletableFuture<Void> allNestedFutures = CompletableFuture.allOf(nestedFutures.toArray(new CompletableFuture[0]));
             allNestedFutures.join();
         }, this.executor);
 
         future.thenRun(this::onFinishListener);
-    }
-
-    private void waitForAllFutures(final List<Future<?>> futures) {
-        futures.forEach(future -> {
-            try {
-                future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        });
     }
 
     @Override
